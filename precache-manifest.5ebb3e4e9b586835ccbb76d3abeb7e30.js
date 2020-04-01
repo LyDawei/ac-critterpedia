4f56b660a500eca0e68816a552808e4da69fe03d
David Ly <david.ly@transcore.com>
2020-04-01 18:46:12 -0500

    Updates

---
+++ precache-manifest.5ebb3e4e9b586835ccbb76d3abeb7e30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5924904f0eb8092ff878493a1b5d68f1",
+    "url": "/ac-critterpedia/index.html"
+  },
+  {
+    "revision": "188144f17942a9f17c26",
+    "url": "/ac-critterpedia/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "794f8ffce3370647ba06",
+    "url": "/ac-critterpedia/static/js/2.23c6d274.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ac-critterpedia/static/js/2.23c6d274.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "188144f17942a9f17c26",
+    "url": "/ac-critterpedia/static/js/main.dc53f6ce.chunk.js"
+  },
+  {
+    "revision": "2690e0a31e569a3c4151",
+    "url": "/ac-critterpedia/static/js/runtime-main.72652d25.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ac-critterpedia/static/media/logo.5d5d9eef.svg"
+  }
+]);
